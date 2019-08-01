50cf554acfa65123eb7c3e36db44ca27ea5d4849
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-01 18:27:32 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Fabrice Etiennette on 01/08/2019.
+//  Copyright © 2019 Fabrice Etiennette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
